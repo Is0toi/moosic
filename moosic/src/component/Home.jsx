a0e456b22e5533dce30a61adc6bed5825689304c
Is0toi <143647830+Is0toi@users.noreply.github.com>
2025-07-19 00:44:40 -0400

    Color theme complete

--- moosic/src/component/Home.jsx
+++ moosic/src/component/Home.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function Home() {
     return (
-        <div className = "background">
-            <div className="title">
-                <h1> Moosic</h1>
+        <div className="background">
+            <div className="home-text">
+                <div className="title">
+                    <h1> Moosic</h1>
+                    <p> Welcome to Moosic where you share daily clips of your favourite songs with friends!</p>
+                </div>
             </div>
         </div>
     )

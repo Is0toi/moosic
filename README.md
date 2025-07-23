d73b145ec9d34e69b7b79c8c628b92bdaf051cd2
Is0toi <143647830+Is0toi@users.noreply.github.com>
2025-07-23 18:19:51 -0400

    ReadMe + looks

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # moosic
-A website where you can share you favourite songs of the day and specific clips of the music
+
+A website where you can cut music into a desirable length 
+
+## Features
+
+- **Audio Upload** - Supports MP3, WAV, and other browser-compatible formats
+- **Precision Selection** - Select exact start/end times (millisecond accuracy)
+- **Region Playback** - Play only the selected portion
+- **Clean UI** - Minimal card design without waveforms
+- **Responsive** - Works on mobile and desktop
+- **NavBar** - Directs you to whever you want to go
+
+## Info
+
+- I used wavesurfer inorder to get the audio waves. I also used their examples to make regions on the audio:
+https://wavesurfer.xyz/examples/?regions.js
--- moosic/src/component/Home.jsx
+++ moosic/src/component/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 
@@ -25,23 +25,23 @@ function Home() {
     }, []);
 
     return (
-        <div className = "home">
-            <section className = "hidden">
-                <h1 className = "home-title"> Welcome to Moosic</h1>
-                <p className = "description"> Discover and share daily doses of music that moves you. Beyond just sharing songs, pinpoint percise beats that truly resonates </p>
-                
+        <div className="home">
+            <section className="hidden">
+                <h1 className="home-title"> Welcome to Moosic</h1>
+                <p className="description"> Cut songs to a desirable length and play it! </p>
+
                 <Link to="/music" class="button">
-                <button className = "join">
-                    Join now!
-                </button>
+                    <button className="join">
+                        Join now!
+                    </button>
                 </Link>
             </section>
-            <section style = {{background:'white'}}className = "hidden">
+            {/* <section style = {{background:'white'}}className = "hidden">
                 <p>hiiiiii</p>
-            </section>
+            </section> */}
         </div>
 
-       
+
     )
 }
 
